18408954ae5bf12d1d6913ebb2663fd6b3796097
Andrea Perugini <alp.perugini@gmail.com>
2024-12-19 19:17:27 +0100

    added gps fucntionality

--- sendpics.ino
+++ sendpics.ino
@@ -8,6 +8,7 @@
 #include "camera.h"
 #include "himax.h"
 #include <base64.h>
+#include <TinyGPS++.h>
 using namespace mbed;
 using namespace rtos;
 using namespace std::chrono_literals;
@@ -23,13 +24,13 @@ FrameBuffer fb(320, 240, 2);
 #define RECORD_DURATION 5
 #define TOTAL_SAMPLES   (SAMPLE_RATE * RECORD_DURATION * NUM_CHANNELS)
 
+#define GPSBaud 9600
+
+
 int16_t audioBuffer[TOTAL_SAMPLES];
 volatile int samplesRead = 0;
 volatile bool audioReady = false;
 
-// Wi-Fi credentials
-// const char* ssid = "Sunrise_2.4GHz_535660";
-// const char* password = "w1F2tm16xnwN";
 
 // Wi-Fi credentials
 const char* ssid = "1+";
@@ -39,17 +40,36 @@ const char* password = "h49iyc4u";
 const char* serverIP = "192.168.86.20";
 const int serverPort = 9999;
 
+String date; // gets time from gps reading
+
 WiFiClient client;
+TinyGPSPlus gps;
 
 Thread cameraThread(osPriorityNormal, 8192, nullptr, "CameraThread");
 Thread audioThread(osPriorityNormal, 8192, nullptr, "AudioThread");
 Thread networkThread(osPriorityNormal, 16384, nullptr, "NetworkThread"); // Increased stack size
+Thread gpsThread(osPriorityNormal, 8192, nullptr, "GpsThread");
 Mutex bufferMutex;
+Mutex gpsMutex;
+
+bool firstGPSFixAttempt = true;
+bool initialGPSFixSuccess = false;
+
+// GPS Timeout values (in milliseconds)
+const unsigned long FIRST_FIX_TIMEOUT = 10000;
+const unsigned long SUBSEQUENT_FIX_TIMEOUT = 1000;
 
 // Chunk size for image transmission
 const size_t CHUNK_SIZE = 512;
 
-// static unsigned char output[110000];
+struct GPSData {
+    bool valid;
+    double latitude;
+    double longitude;
+    String time; // Store only time (HH:MM:SS)
+};
+
+GPSData currentGPSData = {false, 0.0, 0.0, ""}; // Initialize to invalid
 
 void onPDMData() {
     int bytesAvailable = PDM.available();
@@ -78,7 +98,7 @@ void audioTask() {
             ThisThread::sleep_for(10ms);
         }
 
-        bufferMutex.lock(); // i should actually move the rest of the logic here
+        bufferMutex.lock();
         Serial.println("Audio recording complete!");
         bufferMutex.unlock();
 
@@ -101,6 +121,46 @@ void cameraTask() {
     }
 }
 
+void gpsTask() {
+    while (true) {
+        String timeString = "";
+        bool newGPSData = false;
+
+        while (Serial1.available() > 0) {
+            char c = Serial1.read();
+            gps.encode(c);
+            // Serial.write(c); // for debugging
+            // Serial.write(gps.location.lat());
+            // Serial.write(gps.location.lng());
+            // Serial.write(gps.time.hour());
+
+
+            if (gps.location.isUpdated() && gps.location.isValid()) {
+                newGPSData = true;
+
+                // Time Formatting (only HH:MM:SS)
+                if (gps.time.isValid()) {
+                    timeString = (gps.time.hour() < 10 ? "0" : "") + String(gps.time.hour()) + ":" +
+                                 (gps.time.minute() < 10 ? "0" : "") + String(gps.time.minute()) + ":" +
+                                 (gps.time.second() < 10 ? "0" : "") + String(gps.time.second());
+                }
+            }
+        }
+
+        // Update GPS data only if new and valid data is available
+        if (newGPSData) {
+            gpsMutex.lock();
+            currentGPSData.valid = true;
+            currentGPSData.latitude = gps.location.lat();
+            currentGPSData.longitude = gps.location.lng();
+            currentGPSData.time = timeString;
+            gpsMutex.unlock();
+        }
+
+        ThisThread::sleep_for(100ms); // Adjust sleep duration as needed
+    }
+}
+
 void networkTask() {
     while (true) {
         // Wait until we have some data
@@ -114,6 +174,53 @@ void networkTask() {
 
         double rms = sqrt(sumSquared / samplesRead);
         double averageDb = 20.0 * log10(rms + 1e-8);
+
+        GPSData capturedGPSData;
+        unsigned long gpsStartTime = millis();
+        unsigned long gpsTimeout;
+        bool gotGPSFix = false;
+
+        \
+        if (firstGPSFixAttempt) {
+            gpsTimeout = FIRST_FIX_TIMEOUT;
+
+
+            while (millis() - gpsStartTime < gpsTimeout) {
+                gpsMutex.lock();
+                capturedGPSData = currentGPSData;
+                gpsMutex.unlock();
+                if (capturedGPSData.valid) {
+                    gotGPSFix = true;
+                    break;
+                }
+                ThisThread::sleep_for(50ms);
+            }
+
+            firstGPSFixAttempt = false;
+            initialGPSFixSuccess = gotGPSFix;
+        } else if (initialGPSFixSuccess) {
+            gpsTimeout = SUBSEQUENT_FIX_TIMEOUT;
+
+            // Wait for GPS fix only if the initial fix was successful (needed for indoor demo)
+            while (millis() - gpsStartTime < gpsTimeout) {
+                gpsMutex.lock();
+                capturedGPSData = currentGPSData;
+                gpsMutex.unlock();
+                if (capturedGPSData.valid) {
+                    gotGPSFix = true;
+                    break;
+                }
+                ThisThread::sleep_for(50ms);
+            }
+        } else {
+            // Skip GPS entirely if the initial fix was unsuccessful (needed for indoor demo)
+            capturedGPSData.valid = false;
+            capturedGPSData.latitude = 0.0;
+            capturedGPSData.longitude = 0.0;
+            capturedGPSData.time = "";
+        }
+
+
 
         // Connect and send data
         if (!client.connected()) {
@@ -133,11 +240,20 @@ void networkTask() {
 
             const size_t capacity = JSON_OBJECT_SIZE(5) + 100;
             DynamicJsonDocument jsonDoc(capacity);
-            jsonDoc["gps_lat"] = "37.7749";
-            jsonDoc["gps_lon"] = "-122.4194";
+
             jsonDoc["noise"] = String(averageDb, 2);
-            jsonDoc["date"] = "2023-10-01 12:00:00";
             jsonDoc["id"] = "device_123";
+
+            if (capturedGPSData.valid) {
+            jsonDoc["gps_lat"] = capturedGPSData.latitude;
+            jsonDoc["gps_lon"] = capturedGPSData.longitude;
+            jsonDoc["date"] = capturedGPSData.time;
+            } else {
+                Serial.println("GPS data not available. Sending without GPS.");
+                jsonDoc["gps_lat"] = "0";
+                jsonDoc["gps_lon"] = "0";
+                jsonDoc["time"] = "";
+            }
 
             String jsonString;
             serializeJson(jsonDoc, jsonString);
@@ -213,6 +329,12 @@ void networkTask() {
             Serial.println("Disconnected from server");
             }
 
+            if (!initialGPSFixSuccess) {
+              gpsMutex.lock();
+              currentGPSData.valid = false;
+              gpsMutex.unlock();
+            }
+
             bufferMutex.unlock();
             ThisThread::sleep_for(5s);
     }
@@ -237,6 +359,12 @@ void setup() {
     }
     PDM.onReceive(onPDMData);
 
+    // initialize GPS
+    Serial1.begin(GPSBaud);
+    while (!Serial1) {
+      ;
+    }
+
     // Connect to Wi-Fi
     Serial.print("Connecting to Wi-Fi...");
     WiFi.begin(ssid, password);
@@ -253,6 +381,7 @@ void setup() {
     // Start the tasks
     cameraThread.start(cameraTask);
     audioThread.start(audioTask);
+    gpsThread.start(gpsTask);
     networkThread.start(networkTask);
 }
 
